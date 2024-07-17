a9c4e4f837a1a235d33fd1b46d99e721031b9353
klenilsonrox <klenilsoncoc@gmail.com>
2024-07-17 01:23:26 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,58 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import routerAccounts from './routes/accountRoutes.js';
-import routerUsers from './routes/userRoutes.js';
-import routerTransactions from './routes/transactionRoutes.js';
-import routerContact from './routes/contactRoutes.js';
-import authMiddleware from './middlewares/authMiddleware.js';
-import { connectDB } from './database/connectDB.js';
-import { WebSocketServer } from 'ws';
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./database/connectDB.js"
+import routerAccounts from "./routes/accountRoutes.js"
+import routerUsers from "./routes/userRoutes.js"
+import routerRoutes from "./routes/transactionRoutes.js"
+import routerTransactions from "./routes/transactionRoutes.js"
+import routerContact from "./routes/contactRoutes.js"
 
-const app = express();
-app.use(express.json());
-app.use(cors());
 
-app.use('/api', authMiddleware, routerUsers);
-app.use('/api', authMiddleware, routerAccounts);
-app.use('/api', authMiddleware, routerTransactions);
-app.use('/api', authMiddleware, routerContact);
 
-const port = process.env.PORT || 4000;
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-// Configurar o servidor WebSocket
-const wss = new WebSocketServer({ noServer: true });
+app.use("/api", routerUsers)
+app.use("/api", routerAccounts)
+app.use("/api", routerRoutes)
+app.use("/api", routerTransactions)
+app.use("/api", routerContact)
 
-wss.on('connection', (ws) => {
-  console.log('Cliente conectado');
-});
 
-async function startServer() {
-  await connectDB();
 
-  const db = mongoose.connection;
 
-  // Detectar mudanças no MongoDB
-  const changeStream = db.collection('suacolecao').watch();
 
-  changeStream.on('change', (change) => {
-    console.log('Mudança detectada:', change);
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(change));
-      }
-    });
-  });
+connectDB()
 
-  const server = app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-  });
-
-  server.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, (ws) => {
-      wss.emit('connection', ws, request);
-    });
-  });
-}
-
-startServer();
+app.listen(process.env.PORT,()=>{
+    console.log(`servidor rodando na porta ${process.env.PORT}`)
+})
